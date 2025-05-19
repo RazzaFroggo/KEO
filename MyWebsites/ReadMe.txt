60885d9b59a330f8ffd16a885e80af21db2653a6
RazzaFroggo <130469340+RazzaFroggo@users.noreply.github.com>
2025-05-19 13:29:31 +0700

    Add files via upload

---
+++ MyWebsites/ReadMe.txt
@@ -0,0 +1,3 @@
+Here is a CRUV website that connects to the database and will be able to create, edit as well delete the particular data.
+
+And in this one in particular, demonstrates a simple webpage that works with various food items in a restaurant to keep them in check. You'll be able to add items and edit them to your liking, all of which will be stored in the database.
---
+++ MyWebsites/main.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="index.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Restaurant Website</title>
+  </head>
+  <body>
+    
+    
+  <nav>
+    <ul class="nav-flex-row">
+      <li class="nav-item">
+        <a href="#about">About</a>
+      </li>
+      
+      <li class="nav-item">
+        <a href="#menu">Menu</a>
+      </li>
+      
+    </ul>
+  </nav>
+
+  <section class="section-intro">
+    <header>
+      <h1>Smile Burger Shop</h1>
+    </header>
+    <div class="link-to-book-wrapper">
+      <a href="admin_page.php">MENU MANAGEMENT</a>
+    </div>
+  </section>
+
+  <section class="about-section">
+    <article>
+      
+      <p>
+        Hungry? Tired? On the run from a group of angry yakuza? Then it’s time to SMILE
+        because at Smile Burger, we don’t just serve burgers—we serve happiness on a bun!
+
+        And don’t forget our signature Smile Set Meal—complete with fries and a soda so fizzy
+        it’ll jolt you back from the brink of a brawl. Got a heat meter that’s running low? 
+        Recharge with our limited-time Staminan Smile Special—exclusively for fighters, office
+        workers, and heroic ex-yakuza alike.
+      </p>
+    </article>
+  </section>
+
+  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="img/fresh-mini-burgers-on-deerved-600nw-1933379591.jpg" class="d-block w-100" alt="food">
+      </div>
+      <div class="carousel-item">
+        <img src="img/pngtree-juicy-burger-with-cheese-pickles-image_16451368.jpg" class="d-block w-100" alt="food">
+      </div>
+      <div class="carousel-item">
+        <img src="img/brooke-lark-aGjP08-HbYY-unsplash.jpg" class="d-block w-100" alt="food">
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
+  <div class="container">
+    <div class="row-flex">
+      <div class="flex-column-form">
+        <h3>
+          Make a booking
+        </h3>
+      <form class="media-centered">
+          <div class="form-group">
+           
+            <input type="name" class="form-control" id="exampleInputName1" aria-describedby="nameHelp" placeholder="Enter your name">
+          </div>
+          <div class="form-group">
+            <input type="number" class="form-control" id="exampleInputphoneNumber1" placeholder="Enter your phone number">
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+      </div>
+      <div class="opening-time">
+        <h3>
+          Opening times
+        </h3>
+        <p>
+         <span>Monday—Thursday: 08:00 — 22:00</span> 
+         <span>Friday—Saturday: 09:00 — 23:00 </span>
+         <span>Sunday: 10:00 — 17:00</span>
+        </p>
+      </div>
+      <div class="contact-adress">
+        <h3>
+          Contact
+        </h3>
+        <p>
+          <span>XXX XXXX XXXX</span>
+          <span>Nakamichi Street</span>
+          <span>kamurocho</span>
+        </p>
+      </div>
+    </div>
+    </div>
+
+
+
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+ 
+  </body>
+  
+</html> 
